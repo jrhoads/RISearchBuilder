9acc5d0e3ecb178e8b90f8a61866a4cca129dd0c
jrhoads <joseph_rhoads@brown.edu>
2013-05-29 12:11:36 -0400

    Previous attribute hiding attempt was flawed.  Now fixed

--- riSearchBuilder/RISearchBuilder.py
+++ riSearchBuilder/RISearchBuilder.py
@@ -5,59 +5,59 @@ class RISearchBuilder(object):
     def __init__(self):
         """RISearcher initializer"""
         super(RISearchBuilder, self).__init__()
-        self.__and_clauses = set()
-        self.__or_clauses = set()
-        self.__order_by = None
+        self._and_clauses = set()
+        self._or_clauses = set()
+        self._order_by = None
 
     def and_clause(self, clause):
         """Add an additional AND clause"""
-        self.__and_clauses.add(clause)
+        self._and_clauses.add(clause)
         return self
 
     def or_clause(self, clause):
         """Add an additional OR clause"""
-        self.__or_clauses.add(clause)
+        self._or_clauses.add(clause)
         return self
 
-    def order_by(self):
-        return self.__order_by
+    def _get_order_by(self):
+        return self._order_by
 
     def order_by(self, variable):
-        self.__order_by = variable
+        self._order_by = variable
         return self
 
     def serialize(self, joiner=" "):
         """Convert object to a string"""
         stringList = []
-        stringList.append("select %s" % self.__all_selects())
+        stringList.append("select %s" % self._all_selects())
         stringList.append("from <#ri>")
         stringList.append("where")
-        if self.__and_clauses:
-            stringList.append(self.__all_ands())
-        if self.__or_clauses:
-            stringList.append(self.__all_ors())
-        if self.order_by():
-            stringList.append("order by %s" % self.order_by())
+        if self._and_clauses:
+            stringList.append(self._all_ands())
+        if self._or_clauses:
+            stringList.append(self._all_ors())
+        if self._get_order_by():
+            stringList.append("order by %s" % self._get_order_by())
         return joiner.join(stringList)
 
-    def __all_clauses_tokens(self):
-        return " ".join(set(self.__all_clauses().split(' ')))
+    def _all_clauses_tokens(self):
+        return " ".join(set(self._all_clauses().split(' ')))
 
-    def __all_clauses(self):
-        return " ".join(self.__and_clauses | self.__or_clauses)
+    def _all_clauses(self):
+        return " ".join(self._and_clauses | self._or_clauses)
 
-    def __all_selects(self):
+    def _all_selects(self):
         regex = re.compile("(\$\w+)")
-        return " ".join(regex.findall(self.__all_clauses_tokens()))
+        return " ".join(regex.findall(self._all_clauses_tokens()))
 
-    def __all_ands(self):
-        return " and ".join(self.__and_clauses)
+    def _all_ands(self):
+        return " and ".join(self._and_clauses)
 
-    def __all_ors(self):
+    def _all_ors(self):
         temp_ors = ""
-        if self.__or_clauses:
-            temp_ors = " or ".join(self.__or_clauses)
-            if self.__and_clauses:
+        if self._or_clauses:
+            temp_ors = " or ".join(self._or_clauses)
+            if self._and_clauses:
                 temp_ors = "or " + temp_ors
         return temp_ors
 
--- test_RISearchBuilder.py
+++ test_RISearchBuilder.py
@@ -2,8 +2,6 @@ from riSearchBuilder import RISearchBuilder
 
 ri = RISearchBuilder()
 ri.and_clause("$item <fedora-rels-ext:%s> <info:fedora/%s>" % ("isMemberOf", "bdr:2559"))
-ri.and_clause("$item <bul-rel:hasPagination> $page" )
-ri.and_clause("$sub $pred $obj"). \
-        and_clause("$sub $bpred MOO"). \
-        or_clause("$may $or $sub")
+ri.and_clause("$item <bul-rel:hasPagination> $page" ). \
+        order_by("$page")
 print ri.serialize(joiner=' ')
