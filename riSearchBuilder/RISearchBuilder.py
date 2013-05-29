5b42c5a36ee74a54acaafd929bc783fa14de0763
jrhoads <joseph_rhoads@brown.edu>
2013-05-29 11:50:08 -0400

    Separated out the public vs private api

--- riSearchBuilder/RISearchBuilder.py
+++ riSearchBuilder/RISearchBuilder.py
@@ -10,41 +10,6 @@ class RISearchBuilder(object):
         self.or_clauses = set()
         self._order_by = None
 
-    def serialize(self, joiner=" "):
-        """Print out the select statement"""
-        stringList = []
-        stringList.append("select %s" % self.all_selects())
-        stringList.append("from <#ri>")
-        stringList.append("where")
-        if self.and_clauses:
-            stringList.append(self.all_ands())
-        if self.or_clauses:
-            stringList.append(self.all_ors())
-        if self._order_by:
-            stringList.append("order by %s" % self._order_by)
-        return joiner.join(stringList)
-
-    def all_clauses_tokens(self):
-        return " ".join(set(self.all_clauses().split(' ')))
-
-    def all_clauses(self):
-        return " ".join(self.and_clauses | self.or_clauses)
-
-    def all_selects(self):
-        regex = re.compile("(\$\w+)")
-        return " ".join(regex.findall(self.all_clauses_tokens()))
-
-    def all_ands(self):
-        return " and ".join(self.and_clauses)
-
-    def all_ors(self):
-        temp_ors = ""
-        if self.or_clauses:
-            temp_ors = " or ".join(self.or_clauses)
-            if self.and_clauses:
-                temp_ors = "or " + temp_ors
-        return temp_ors
-
     def and_clause(self, clause):
         """Add an additional AND clause"""
         self.and_clauses.add(clause)
@@ -58,3 +23,39 @@ class RISearchBuilder(object):
     def order_by(self, variable):
         self._order_by = variable
         return self
+
+    def serialize(self, joiner=" "):
+        """Convert object to a string"""
+        stringList = []
+        stringList.append("select %s" % self.__all_selects())
+        stringList.append("from <#ri>")
+        stringList.append("where")
+        if self.and_clauses:
+            stringList.append(self.__all_ands())
+        if self.or_clauses:
+            stringList.append(self.__all_ors())
+        if self._order_by:
+            stringList.append("order by %s" % self._order_by)
+        return joiner.join(stringList)
+
+    def __all_clauses_tokens(self):
+        return " ".join(set(self.__all_clauses().split(' ')))
+
+    def __all_clauses(self):
+        return " ".join(self.and_clauses | self.or_clauses)
+
+    def __all_selects(self):
+        regex = re.compile("(\$\w+)")
+        return " ".join(regex.findall(self.__all_clauses_tokens()))
+
+    def __all_ands(self):
+        return " and ".join(self.and_clauses)
+
+    def __all_ors(self):
+        temp_ors = ""
+        if self.or_clauses:
+            temp_ors = " or ".join(self.or_clauses)
+            if self.and_clauses:
+                temp_ors = "or " + temp_ors
+        return temp_ors
+
